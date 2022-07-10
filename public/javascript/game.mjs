354925732f1b6522697358e99ec9383af1de44a8
Laptop <babalielchin@gmail.com>
2022-07-10 22:42:57 +0400

    references of modules

--- public/javascript/game.mjs
+++ public/javascript/game.mjs
@@ -1,7 +1,24 @@
+import { showInputModal } from "../views/modal.mjs";
+import { appendRoomElement } from "../views/room.mjs";
+
 const username = sessionStorage.getItem('username');
 
 if (!username) {
 	window.location.replace('/login');
 }
 
+const addRoomBtn = document.getElementById('add-room-btn');
+
 const socket = io('', { query: { username } });
+
+const addRoom = () => {
+	// socket.emit("create-room");
+    appendRoomElement({ name: "default", numberOfUsers: 0 });
+};
+
+const onClickAddRoomBtn = () => {
+    showInputModal({ title: 'Add Room', onSubmit: addRoom });
+};
+
+addRoomBtn.addEventListener('click', onClickAddRoomBtn);
+
