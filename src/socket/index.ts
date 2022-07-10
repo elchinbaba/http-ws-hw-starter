a64dd673706ef37a6bccc810200bd48412e539e5
Laptop <babalielchin@gmail.com>
2022-07-10 22:44:30 +0400

    create room1

--- src/socket/index.ts
+++ src/socket/index.ts
@@ -4,5 +4,9 @@ import * as config from './config';
 export default (io: Server) => {
 	io.on('connection', socket => {
 		const username = socket.handshake.query.username;
+
+		// io.of("/").adapter.on("create-room", (room: string) => {
+		// 	console.log(`room ${room} was created`);
+		// });
 	});
 };
